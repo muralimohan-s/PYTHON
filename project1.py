7d452c41fd4361e26ba3d362cf7261d3d8702620
muralimohan-s <muralimohan_s@srmap.edu.in>
2021-03-25 13:25:56 +0530

    version1.1

--- project1.py
+++ project1.py
@@ -2,6 +2,56 @@
 """
 Created on Fri Mar 19 14:36:10 2021
 
-@author: MuraliMohan
+@author: MuraliMohan s
+
 """
 
+import random
+
+
+game_list = ["stone", "paper", "scissors"]
+game_list_quit = ["stone", "paper", "scissors", "quit"]
+computer_score = 0
+player_score = 0
+print("score:computer = {x}  score:player = {y}".format(x=computer_score, y=player_score))
+
+while True:
+    computer_choice = random.choice(game_list)
+    print("\n",game_list)
+    command = input("please select your choice from the list\n")
+    if command in game_list_quit:
+        if command == computer_choice:
+            print("Tie")
+        elif command == "stone":
+            if computer_choice == "scissors":
+                print("player won!")
+                player_score += 1
+            else:
+                print("computer won!")
+                computer_score += 1
+        elif command == "paper":
+            if computer_choice == "stone":
+                print ("Player won!")
+                player_score += 1
+            else:
+                print("computer won!")
+                computer_score += 1
+        elif command == "scissors":
+            if computer_choice == "paper":
+                print("player won!")
+                player_score += 1
+            else:
+                print("computer won!")
+                computer_score += 1
+        elif command == "quit":
+            if player_score > computer_score:
+                print("player won the game, player score:{x}, computer score:{y}".format(x=str(player_score), y=str(computer_score)))
+                break
+            elif computer_score > player_score:
+                print("computer won the game, player score:{x}, computer score:{y}".format(x=str(player_score), y=str(computer_score)))
+                break
+            else:
+                print("tie, player score:{x}, computer score:{y}".format(x=str(player_score), y=str(computer_score)))
+                break
+    else:
+        print("invalid command")
